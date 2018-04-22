190b8e99cfb4a66e40f03357a5e5a47a1f66d3a2
jrhemmin <jrhemming@asu.edu>
2018-04-22 12:44:27 -0700

    changes page color

--- ser322/public/read.php
+++ ser322/public/read.php
@@ -1,5 +1,34 @@
 <?php include "templates/header.php"; ?>
 
+<!DOCTYPE php>
+<php lang="en">
+<echo> <body style="background-color:teal"></echo>
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body> 
+ 
+<div class="container">
+  <h2>Petsposion</h2>
+  <div class="panel panel-default">
+    <div class="panel-body"><ul class="nav nav-pills nav-stacked">
+  <li cla
+  ss="active"><a href="#">Home</a></li>
+  <li><a href="#">Cats</a></li>
+  <li><a href="#">Dogs</a></li>
+  <li><a href="#">Shelters</a></li>
+</ul></div>
+  </div>
+</div>
+
+</body>
+</php>
+
 <h2>Find animals based on pet type</h2>
 
 <form method="post">
@@ -32,7 +61,7 @@
 
 <h2>Find pets based on color</h2>
 <form method = "post">
-	<label> for= "color"Pet Color</label>
+	<label for= "color">Pet Color</label>
 	<input type = "text" id="color" name = "color">
 	<input type = "submit" name = "submit" value ="View Results">
 </form>
