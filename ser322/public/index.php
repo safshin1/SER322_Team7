73409e93cd06e4ccabfbeba88ef927880c820ec1
jrhemmin <jrhemming@asu.edu>
2018-04-22 19:06:58 -0700

    small changes

--- ser322/public/index.php
+++ ser322/public/index.php
@@ -1,5 +1,21 @@
 <?php include 'templates/header.php';?>	
-	
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+
+  <div class="panel panel-default">
+  <div class="panel-body"><ul class="nav nav-pills nav-stacked">
+  <li class="active"><a href="#">Home</a></li>
+  <li><a href="#">Cats</a></li>
+  <li><a href="#">Dogs</a></li>
+  <li><a href="#">Shelters</a></li>
+</ul></div>
+  </div>
 	<ul>
 		<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
 		<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
--- ser322/public/read.php
+++ ser322/public/read.php
@@ -8,8 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-
-  <h2>Petsposion</h2>
+<div class="container">
   <div class="panel panel-default">
     <div class="panel-body"><ul class="nav nav-pills nav-stacked">
   <li class="active"><a href="#">Home</a></li>
@@ -22,7 +21,7 @@
 
 
 
-<h2>Find animals based on pet type</h2>
+<h3>Find animals based on pet type</h3>
 
 <form method="post">
 	<label for="type">Pet type</label>
@@ -30,7 +29,7 @@
 	<input type="submit" name="submit" value="View Results">
 </form>
 
-<h2>Find pets based on age</h2>
+<h3>Find pets based on age</h3>
 
 <form method = "post">
 	<label for= "age">Pet Age</label>
@@ -38,28 +37,28 @@
 	<input type = "submit" name = "submit" value ="View Results">
 </form>
 
-<h2>Find pets based on breed</h2>
+<h3>Find pets based on breed</h3>
 <form method = "post">
 	<label for= "breed">Pet Breed</label>
 	<input type = "text" id="breed" name = "breed">
 	<input type = "submit" name = "submit" value ="View Results">
 </form>
 
-<h2>Find pets based on location</h2>
+<h3>Find pets based on location</h3>
 <form method = "post">
 	<label for= "location">Pet Location</label>
 	<input type = "text" id="location" name = "location">
 	<input type = "submit" name = "submit" value ="View Results">
 </form>
 
-<h2>Find pets based on color</h2>
+<h3>Find pets based on color</h3>
 <form method = "post">
 	<label for= "color">Pet Color</label>
 	<input type = "text" id="color" name = "color">
 	<input type = "submit" name = "submit" value ="View Results">
 </form>
 
-<h2>Find pets based on type and color</h2>
+<h3>Find pets based on type and color</h3>
 <form method = "post">
 	<label for = "type">Pet Type and Color</label>
 	<input type="text" id="type" name="type">
@@ -68,4 +67,5 @@
 <a href="index.php">Back to home</a>
 </body>
 </html>
+
 
