96cf49d32b4d0138da0286cf810d4d301c75d10d
JJanas <JJanas@asu.edu>
2018-04-24 09:46:12 -0700

    Rearranged the tables

--- data/init.sql
+++ data/init.sql
@@ -2,66 +2,71 @@ CREATE DATABASE PETSPLOSION;
 
 use test;
 
+CREATE TABLE CONTACT_INFO(
+	contactInfoID int NOT NULL,
+	addressName varchar(15),
+	street varchar(15),
+	state varchar(15),
+	zip int,
+	email varchar(15) NOT NULL,
+	phone int NOT NULL,
+	PRIMARY KEY(contactInfoID)
+);
+
 CREATE TABLE ADOPTERS (
-	aID INT NOT NULL,
-	fName VARCHAR(15) NOT NULL,
-	lName VARCHAR(15) NOT NULL,
-	contactInfoID INT NOT NULL,
+	aID int NOT NULL,
+	fName varchar(15) NOT NULL,
+	lName varchar(15) NOT NULL,
+	contactInfoID int NOT NULL,
 	PRIMARY KEY(aID),
-	FOREIGN KEY(contactInfoID) references CONTACT_INFO(contactInfoID)
-);
-
-CREATE TABLE PETS (
-	petID INT NOT NULL,
-	petName VARCHAR(15) NOT NULL,
-	breedID VARCHAR(15) NOT NULL,
-	age INT,
-	sex CHAR(1),
-	locID INT,
-	dateRecieved DATE,
-	intakeAgent VARCHAR(15) NOT NULL,
-	adoptionID INT
-	PRIMARY KEY(petID),
-	FOREIGN KEY(breedID) references BREEDS(breedID),
-	FOREIGN KEY(locID) references LOCATIONS(locID),
-	FOREIGN KEY(adoptionID) references ADOPTION_INFO(adoptionID)
-);
-
-CREATE TABLE ADOPTION_INFO (
-	adoptionID INT NOT NULL,
-	dateAdopted DATE,
-	adoptionAgent VARCHAR(15) NOT NULL,
-	adopterID INT NOT NULL,
-	PRIMARY KEY(adoptionID),
-	FOREIGN KEY(adopterID) references ADOPTERS(aID)
+	FOREIGN KEY(contactInfoID) REFERENCES CONTACT_INFO(contactInfoID)
 );
 
 CREATE TABLE BREEDS (
-	breedID INT NOT NULL,
-	breedName VARCHAR(15) NOT NULL,
-	petType VARCHAR(8) NOT NULL,
-	size VARCHAR(8),
-	color VARCHAR(8),
-	demeanor VARCHAR(15),
+	breedID int NOT NULL,
+	breedName varchar(15) NOT NULL,
+	petType varchar(8) NOT NULL,
+	size varchar(8),
+	color varchar(8),
+	demeanor varchar(15),
 	PRIMARY KEY(breedID)
 );
 
-CREATE TABLE VET_RECORDS (
-	petID INT NOT NULL,
-	vetRecID INT NOT NULL,
-	vacRecID INT NOT NULL,
-	weight INT,
-	issue1 DATE,
-	issue2 DATE,
-	issue3 DATE,
-	visitDate DATE NOT NULL,
-	PRIMARY KEY(vetRecID),
-	FOREIGN KEY(petID) references PETS(petID)
-	FOREIGN KEY(vacRecID) references VACCINATION_RECORDS(vacRecID)
+CREATE TABLE LOCATIONS(
+	locID int NOT NULL,
+	locName varchar(25),
+	contactInfoID int NOT NULL,
+	PRIMARY KEY(locID),
+	FOREIGN KEY(contactInfoID) REFERENCES CONTACT_INFO(contactInfoID)
+);
+
+CREATE TABLE ADOPTION_INFO (
+	adoptionID int NOT NULL,
+	dateAdopted DATE,
+	adoptionAgent varchar(15) NOT NULL,
+	adopterID int NOT NULL,
+	PRIMARY KEY(adoptionID),
+	FOREIGN KEY(adopterID) REFERENCES ADOPTERS(aID)
+);
+
+CREATE TABLE PETS (
+	petID int NOT NULL,
+	petName varchar(15) NOT NULL,
+	breedID varchar(15) NOT NULL,
+	age int,
+	sex CHAR(1),
+	locID int,
+	dateRecieved DATE,
+	intakeAgent varchar(15) NOT NULL,
+	adoptionID int
+	PRIMARY KEY(petID),
+	FOREIGN KEY(breedID) REFERENCES BREEDS(breedID),
+	FOREIGN KEY(locID) REFERENCES LOCATIONS(locID),
+	FOREIGN KEY(adoptionID) REFERENCES ADOPTION_INFO(adoptionID)
 );
 
 CREATE TABLE VACCINATION_RECORDS(
-	vacRecID INT NOT NULL
+	vacRecID int NOT NULL
 	rabies DATE,
 	parvo DATE,
 	distemper DATE,
@@ -74,35 +79,38 @@ CREATE TABLE VACCINATION_RECORDS(
 	PRIMARY KEY(vacRecID)
 );
 
-CREATE TABLE CONTACT_INFO(
-	contactInfoID INT NOT NULL,
-	addressName VARCHAR(15),
-	street VARCHAR(15),
-	state VARCHAR(15),
-	zip INT,
-	email VARCHAR(15) NOT NULL,
-	phone INT NOT NULL,
-	PRIMARY KEY(contactInfoID)
+CREATE TABLE VET_RECORDS (
+	petID int NOT NULL,
+	vetRecID int NOT NULL,
+	vacRecID int NOT NULL,
+	weight int,
+	issue1 DATE,
+	issue2 DATE,
+	issue3 DATE,
+	visitDate DATE NOT NULL,
+	PRIMARY KEY(vetRecID),
+	FOREIGN KEY(petID) REFERENCES PETS(petID)
+	FOREIGN KEY(vacRecID) REFERENCES VACCINATION_RECORDS(vacRecID)
 );
 
 CREATE TABLE EMPLOYEES(
-	empID INT NOT NULL,
-	fName VARCHAR(15) NOT NULL,
-	lName VARCHAR(15) NOT NULL,
-	contactInfoID INT NOT NULL,
-	empPosition VARCHAR(10),
-	locID INT,
+	empID int NOT NULL,
+	fName varchar(15) NOT NULL,
+	lName varchar(15) NOT NULL,
+	contactInfoID int NOT NULL,
+	empPosition varchar(10),
+	locID int,
 	PRIMARY KEY(empID),
-	FOREIGN KEY(contactInfoID) references CONTACT_INFO(contactInfoID),
-	FOREIGN KEY(locID) references LOCATIONS(locID)
+	FOREIGN KEY(contactInfoID) REFERENCES CONTACT_INFO(contactInfoID),
+	FOREIGN KEY(locID) REFERENCES LOCATIONS(locID)
 );
 
 CREATE TABLE LOCATIONS(
-	locID INT NOT NULL,
-	locName VARCHAR(25),
-	contactInfoID INT NOT NULL,
+	locID int NOT NULL,
+	locName varchar(25),
+	contactInfoID int NOT NULL,
 	PRIMARY KEY(locID),
-	FOREIGN KEY(contactInfoID) references CONTACT_INFO(contactInfoID)
+	FOREIGN KEY(contactInfoID) REFERENCES CONTACT_INFO(contactInfoID)
 );
 
 
